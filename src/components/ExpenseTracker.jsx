d031be86526db6708efa546f5262bed4492ed903
Madhav Gandhasiri <madhavgandhasiri@gmail.com>
2025-04-08 17:13:03 +0530

    removed div in header

--- src/components/ExpenseTracker.jsx
+++ src/components/ExpenseTracker.jsx
@@ -89,9 +89,9 @@ function ExpenseTracker() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
+      {/* <div className={styles.header}> */}
         <h1>Expense Tracker</h1>
-      </div>
+      {/* </div> */}
       <div className={styles.upper}>
         <div className={styles.wallet}>
           <p>Wallet Balance: ₹{balance}</p>
